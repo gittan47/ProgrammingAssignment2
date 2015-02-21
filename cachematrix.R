ed51a24504e8083c02c747c4f5f45a06123ffe39
gittan47 <aniella.beser@ki.se>
2015-02-21 23:24:09 +0100

    Assignment 2 in the R-programming course
    Caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+#MY ASSIGNMENT 2 in the R-programming course
+#Caching the Inverse of a Matrix
+# Matrix inversion is usually a costly computation and there may be some benefit 
+# to caching the inverse of a matrix rather than compute it repeatedly. The 
+# following two functions are used to cache the inverse of a matrix. 
+# makeCacheMatrix creates a list containing a function to 
+# 1. set the value of the matrix 
+# 2. get the value of the matrix 
+# 3. set the value of inverse of the matrix 
+# 4. get the value of inverse of the matrix 
 
-## Write a short comment describing this function
+#Here I construct a testmatrix to use for the computations
+x<-matrix(rnorm(100),nrow=10,ncol=10)
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+  inv <- NULL 
+  set <- function(y) { 
+    x <<- y 
+    inv <<- NULL 
+  } 
+  get <- function() x 
+  setinverse <- function(inverse) inv <<- inverse 
+  getinverse <- function() inv 
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
+} 
+#I'm curious to see how it looks in the environment
+xx <- makeCacheMatrix(x)
+ls.str(environment(xx$get))
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# The following function returns the inverse of the matrix. It first checks if 
+# the inverse has already been computed. If so, it gets the result and skips the 
+# computation. If not, it computes the inverse, sets the value in the cache via 
+# setinverse function. 
+# This function assumes that the matrix is always invertible. 
+cacheSolve <- function(x, ...) { 
+  inv <- x$getinverse() 
+  if(!is.null(inv)) { 
+    message("getting cached data.") 
+    return(inv) 
+  } 
+  data <- x$get() 
+  inv <- solve(data) 
+  x$setinverse(inv) 
+  inv 
+} 
+#Here too is to take a look at what's happening
+cacheSolve(xx)
+ls.str(environment(xx$get))
